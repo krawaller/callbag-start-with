ae44a2ba87ce949b063c9c9cd36b9318a9455ce7
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-07-18 00:15:51 +0200

    Refactor fix for stopping data emits after unsubscribe

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const startWith = (...xs) => inputSource => (start, outputSink) => {
   xs = xs.map(v => [1, v]);
   let inputTalkback;
   let inited = false;
-  let unsubed = false;
   inputSource(0, (it, id) => {
     if (it === 0){
       inputTalkback = id;
 
       outputSink(0, (ot, od) => {
         if (ot === 0) return;
-        if (ot === 2) unsubed = true;
+        if (ot === 2) xs.length = 0;
         inputTalkback(ot, od);
       });
 
-      while (xs.length !== 0 && !unsubed) {
+      while (xs.length !== 0) {
         outputSink(...xs.shift());
       }
 
