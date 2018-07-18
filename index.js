2db54869e187f91531e2c0e257a1df6f2ffca906
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-07-18 11:16:12 +0200

    Move inputTalkback declaration closer to greeting outputSink

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 const startWith = (...xs) => inputSource => (start, outputSink) => {
   if (start !== 0) return;
   xs = xs.map(v => [1, v]);
-  let inputTalkback;
   let inited = false;
   let completed = false;
   inputSource(0, (it, id) => {
     if (it === 0){
-      inputTalkback = id;
+      const inputTalkback = id;
 
       outputSink(0, (ot, od) => {
         if (ot === 0) return;
